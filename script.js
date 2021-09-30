f75e8fc8277f3bd407cad1d9e413b4147c3bfca6
Abby <79819338+dinowaffles@users.noreply.github.com>
2021-09-29 23:29:53 -0500

    completed the compute function

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 function compute() {
     p = document.getElementById("principal").value;
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate /100;
+    let year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML = `If you deposit ${principal}, at an interest rate of ${rate}. You will receive an amount of ${interest} in the year ${year}.`;
 }
-
-const principal = document.getElementById("principal").value;
-
-const rate = document.getElementById("rate").value;
-
-let years = document.getElementById("years").value;
-
-let interest = principal * years * rate /100;
-
-let year = new Date().getFullYear()+parseInt(years);
 
 function updateRate() {
     let rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
-
-document.getElementById("result").innerHTML = `If you deposit ${principal}, at an interest rate of ${rate}. You will receive an amount of ${interest} in the year ${year}.`;
-        
