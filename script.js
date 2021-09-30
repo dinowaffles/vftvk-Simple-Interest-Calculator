3570bdb71cad01d920c5409848f7b45dcb1eafbe
Abby <79819338+dinowaffles@users.noreply.github.com>
2021-09-29 22:41:31 -0500

    add js to slider

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+const principal = document.getElementById("principal").value;
+const rate = document.getElementById("rate").value;
+const years = document.getElementById("years").value;
+
+let interest = principal * years * rate /100;
+
+let year = new Date().getFullYear()+parseInt(years);
+
+function updateRate() {
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
+
+function compute() {
+    p = document.getElementById("principal").value;
+}
+
         
